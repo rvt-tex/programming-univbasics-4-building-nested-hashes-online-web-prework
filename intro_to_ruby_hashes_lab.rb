7246ec949c2e254118c001ce8755d36ffe30d03c
Ruel Tingling <rvtingling@yahoo.com>
2019-09-02 10:11:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ def base_hash = {
       :Germany => {},
       :USA => {}
    },
-   :Caribbean_island => {
+   :Caribbean_islands => {
       :Bahamas => {},
       :Cayman => {},
       :Barbados => {},
@@ -18,6 +18,20 @@ def base_hash = {
 end
 
 def monopoly_with_second_tier
+  epic_tragedy = {
+   :world_countries => {
+      :China => {population: "1.4 billion", language: "mandarin"},
+      :England => {population: "55.6 million", language: "English"},
+      :Germany => {population: "82.8 million", language: "german deutsh"},
+      :USA => {population: "327.2 million", language: "English"}
+   },
+   :Caribbean_islands => {
+      :Bahamas => {name: "Lord Capulet", age: "50"},
+      :Cayman => {name: "Lady Capulet", age: "51"},
+      :Barbados => {name: "Juliet", age: "15", status: "alive"},
+      :Jamaica => {}
+   }
+}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
